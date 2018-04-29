edffdedd7e8c438814859c117a05d572d2e51548
qh384@nyu.edu <qh384@nyu.edu>
2018-04-29 17:08:29 -0400

    model for downstream tasks updated

---
+++ project/order/main.py
@@ -0,0 +1,265 @@
+from __future__ import unicode_literals, print_function, division
+
+import argparse
+import time
+from io import open
+import random
+import math
+import numpy as np
+import pickle as pkl
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.backends.cudnn as cudnn
+from torch.autograd import Variable
+
+from utils import *
+from model import *
+from metric import score
+
+###############################################
+# Training settings
+###############################################
+
+parser = argparse.ArgumentParser(description='Sentence Reconstruction with NGrams')
+parser.add_argument('--order', type=int, default='3', metavar='N',
+                    help='order of ngram (default: 3)')
+parser.add_argument('--data-path', type=str, default='.', metavar='PATH',
+                    help='data path of pairs.pkl and lang.pkl (default: current folder)')
+parser.add_argument('--mode', type=str, choices=['sum', 'mean'], default='sum', metavar='MODE',
+                    help='mode of bag-of-n-gram representation (default: sum)')
+parser.add_argument('--metric', type=str, default='ROUGE', metavar='METRIC',
+                    help='metric to use (default: ROUGE; BLEU and BLEU_clip available)')
+parser.add_argument('--num-words', type=int, default='10000', metavar='N',
+                    help='maximum ngrams vocabulary size to use (default: 10000')
+parser.add_argument('--hidden-size', type=int, default='256', metavar='N',
+                    help='hidden size (default: 256)')
+parser.add_argument('--n-epochs', type=int, default=1, metavar='N',
+                    help='number of epochs to train (default: 1)')
+parser.add_argument('--n-iters', type=int, default=3000, metavar='N',
+                    help='number of iters to train (default: 3000), for testing only')
+parser.add_argument('--print-every', type=int, default='1000', metavar='N',
+                    help='print every (default: 1000) iters')
+parser.add_argument('--plot-every', type=int, default='100', metavar='N',
+                    help='plot every (default: 100) iters')
+parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                    help='learning rate (default: 0.01)')
+parser.add_argument('--max-length', type=int, default='100', metavar='N',
+                    help='max-ngrams-length (set by preprocessing)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=1, metavar='S',
+                    help='random seed (default: 1)')
+
+
+###############################################
+# Preparing training data
+###############################################
+
+class Lang:
+    def __init__(self, lang_load):
+        self.word2index = lang_load[0]
+        self.word2count = lang_load[1]
+        self.index2word = lang_load[2]
+        self.n_words = lang_load[3]
+        self.order = lang_load[4]
+        self.vocab_ngrams = lang_load[5]
+        self.max_ngrams_len = lang_load[6]
+
+def indexesFromNGramList(vocab, ngram_list, num_words):
+    result = []
+    for ng in ngram_list:
+        if ng in vocab:
+            idx = vocab[ng]
+            if idx > num_words:
+                result.append(UNK_token)
+            else:
+                result.append(idx)
+        else:
+            result.append(UNK_token)
+    return result
+
+def variableFromNGramList(vocab, ngram_list, num_words, args):
+    indexes = indexesFromNGramList(vocab, ngram_list, num_words)
+    result = Variable(torch.LongTensor(indexes).view(-1, 1))
+    if args.cuda:
+        return result.cuda()
+    else:
+        return result
+
+def variableFromSentenceLength(label, args):
+    length = int(label)
+    # bins: (1-4), (5-8), (9-12), (13-16), (17-20), (20-24), (24+)
+    category = min(math.floor((length - 1) / 4), 6)
+    result = Variable(torch.LongTensor([category]))
+    if args.cuda:
+        return result.cuda()
+    else:
+        return result
+    
+    
+def variablesFromPair(pair, lang, args):
+    input_variable = variableFromNGramList(lang.vocab_ngrams, pair[0], args.num_words, args)
+    target_variable = variableFromSentenceLength(pair[1], args)
+    return (input_variable, target_variable)
+
+
+###############################################
+# Training
+###############################################
+
+def train(input_variable, target_variable, encoder, decoder, net_optimizer, criterion, args):
+    use_cuda = args.cuda
+    max_length = args.max_length
+
+    loss = 0
+
+    # encoder_optimizer.zero_grad()
+    net_optimizer.zero_grad()
+
+    encoder_hidden = encoder(input_variable)
+    net_output = net(encoder_hidden)
+    loss = criterion(net_output, target_variable)
+    loss.backward()
+
+    # encoder_optimizer.step()
+    net_optimizer.step()
+
+    return loss.data[0]
+
+def trainEpochs(encoder, net, lang, pairs, args):
+    n_epochs = args.n_epochs
+    print_every = args.print_every
+    plot_every = args.plot_every
+    learning_rate = args.lr
+
+    start = time.time()
+    iter = 0
+    n_iters = n_epochs * len(pairs)
+    plot_losses = []
+    print_loss_total = 0  # Reset every print_every
+    plot_loss_total = 0  # Reset every plot_every
+
+    # encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    net_optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+    criterion = nn.CrossEntropyLoss()
+
+    for epoch in range(1, args.n_epochs + 1):
+        random.shuffle(pairs)
+        training_pairs = [variablesFromPair(pair, lang, args)
+                      for pair in pairs]
+
+        for training_pair in training_pairs:
+            iter += 1
+            input_variable = training_pair[0]
+            target_variable = training_pair[1]
+
+            loss = train(input_variable, target_variable, encoder,
+                    net, net_optimizer, criterion, args)
+            print_loss_total += loss
+            plot_loss_total += loss
+
+            if iter % args.print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                            iter, iter / n_iters * 100, print_loss_avg))
+
+            if iter % args.plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
+
+        print("Epoch {}/{} finished".format(epoch, args.n_epochs))
+
+    showPlot(plot_losses, args.order)
+
+
+###############################################
+# Evaluation
+###############################################
+
+def evaluateRandomly(encoder, net, pairs, lang, args, n=10):
+    for i in range(n):
+        pair = random.choice(pairs)
+        print('>', pair[0])
+        print('=', pair[1])
+
+        input_variable = variableFromNGramList(lang.vocab_ngrams, pair[0], args.num_words, args)
+        input_length = input_variable.size()[0]
+
+        encoder_hidden = encoder(input_variable)
+
+        outputs = net(encoder_hidden)
+        predict = torch.max(outputs, 1)[1].data[0]
+
+        if predict < 6:
+            print('< {}-{}'.format(predict * 4 + 1, predict * 4 + 4))
+        else:
+            print('< 24+')
+        print('')
+
+def evaluateTestingPairs(encoder, net, pairs, lang, args):
+    use_cuda = args.cuda
+    max_length = args.max_length
+
+    correct = 0
+    total = 0
+    for pair in pairs:
+        input_variable = variableFromNGramList(lang.vocab_ngrams, pair[0], args.num_words, args)
+        input_length = input_variable.size()[0]
+
+        encoder_hidden = encoder(input_variable)
+
+        outputs = net(encoder_hidden)
+        predict = torch.max(outputs, 1)[1].data[0]
+        label = min(math.floor((int(pair[1]) - 1) / 4), 6)
+        
+        total += 1
+        if (predict == label):
+            correct += 1
+
+    print('Accuracy of the network on the sentence length test set: %d %%' % (
+        100 * correct / total))
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    # Set the seed for generating random numbers
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+
+    # Load pairs.pkl and lang.pkl
+    with open(args.data_path + "/pairs%d.pkl" % args.order, 'rb') as f:
+        (train_pairs, test_pairs) = pkl.load(f)
+    with open(args.data_path + "/lang%d.pkl" % args.order, 'rb') as f:
+        lang_load = pkl.load(f)
+    lang = Lang(lang_load)
+    args.max_length = lang.max_ngrams_len
+
+    # Set encoder and net
+    net = MLP(args.hidden_size, class_size=7)
+    encoder = NGramEncoder(args.num_words, args.hidden_size, args.mode)
+    if args.cuda:
+        encoder = encoder.cuda()
+        net = net.cuda()
+
+    # Load pretrained embedding weights
+    with open("embedding_weights%d.pkl" % args.order, 'rb') as f:
+        embedding_weights = pkl.load(f)
+        encoder.embeddingBag.weight.data.copy_(torch.from_numpy(embedding_weights))
+
+    # Train and evalute
+    print("Evaluate randomly on training sentences:")
+    evaluateRandomly(encoder, net, train_pairs, lang, args)
+    print("Evaluate randomly on testing sentences:")
+    evaluateRandomly(encoder, net, test_pairs, lang, args)
+    trainEpochs(encoder, net, lang, train_pairs, args)
+    print("Evaluate randomly on training sentences:")
+    evaluateRandomly(encoder, net, train_pairs, lang, args)
+    print("Evaluate randomly on testing sentences:")
+    evaluateRandomly(encoder, net, test_pairs, lang, args)
+    evaluateTestingPairs(encoder, net, test_pairs, lang, args)
---
+++ project/order/preprocess.py
@@ -0,0 +1,169 @@
+from __future__ import unicode_literals, print_function, division
+
+import argparse
+from io import open
+from collections import OrderedDict
+import random
+import numpy as np
+import pickle as pkl
+
+from utils import *
+
+###############################################
+# Preprocessing settings
+###############################################
+
+parser = argparse.ArgumentParser(description='Data Preprocessing')
+parser.add_argument('--data-path', type=str, default='../data', metavar='PATH',
+                    help='data path (default: ../data)')
+parser.add_argument('--order', type=int, default=3, metavar='N',
+                    help='order of ngram')
+parser.add_argument('--no-filter-pair', dest='filter-pair', action='store_false',
+                    help='disable pair filtering (default: enabled)')
+parser.set_defaults(filter_pair=True)
+parser.add_argument('--max-length', type=int, default=10, metavar='N',
+                    help='maximum length of sentences, available only if pair filtering enabled (default: 10)')
+
+
+###############################################
+# Auxiliary functions for data preprocessing
+###############################################
+
+eng_prefixes = (
+    "i am ", "i m ",
+    "he is", "he s ",
+    "she is", "she s",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+def filterPair(p, max_length):
+    return len(p[1].split(' ')) < max_length and p[1].startswith(eng_prefixes)
+
+def filterPairs(pairs, max_length):
+    return [pair for pair in pairs if filterPair(pair, max_length)]
+
+
+###############################################
+# Core classes and functions
+###############################################
+
+class Lang:
+    def __init__(self, name, order):
+        self.name = name
+        
+        # for single words
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {0: "UNK", 1: "SOS", 2: "EOS"}
+        self.n_words = 3  # Count UNK, SOS and EOS
+        
+        # for ngrams
+        self.order = order
+        self.vocab0 = OrderedDict()
+
+    def addSentence(self, sent):
+        for word in sent.split(' '):
+            self.addWord(word)
+        ngrams = extract_ngrams(sent, self.order)
+        for ng in ngrams:
+            if ng in self.vocab0:
+                self.vocab0[ng] += 1
+            else:
+                self.vocab0[ng] = 1
+        return ngrams
+
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.n_words
+            self.word2count[word] = 1
+            self.index2word[self.n_words] = word
+            self.n_words += 1
+        else:
+            self.word2count[word] += 1
+    
+    def createNGramDictionary(self):
+        tokens = list(self.vocab0.keys())
+        freqs = list(self.vocab0.values())
+        sidx = np.argsort(freqs)[::-1]
+        vocab = OrderedDict([(tokens[s], i) for i, s in enumerate(sidx)])
+        return vocab
+
+def readLangs(lang1, lang2, order, data_path, reverse=False):
+    print("Reading lines...")
+
+    # Read the file and split into lines
+    train_lines = open(data_path + '/train.txt', encoding='utf-8').\
+        read().strip().split('\n')
+    test_lines = open(data_path + '/test.txt', encoding='utf-8').\
+        read().strip().split('\n')
+
+    # Split every line into pairs and normalize
+    train_pairs = [[normalizeString(s) for s in l.split('\t')] for l in train_lines]
+    test_pairs = [[normalizeString(s) for s in l.split('\t')] for l in test_lines]
+
+    # Reverse pairs, make Lang instances
+    if reverse:
+        train_pairs = [list(reversed(p)) for p in train_pairs]
+        test_pairs = [list(reversed(p)) for p in test_pairs]
+        input_lang = Lang(lang2, order)
+        output_lang = Lang(lang1, order)
+    else:
+        input_lang = Lang(lang1, order)
+        output_lang = Lang(lang2, order)
+
+    return input_lang, output_lang, train_pairs, test_pairs
+
+def prepareData(lang1, lang2, order, data_path, filter_pair, max_length, reverse=False):
+    input_lang, output_lang, train_pairs, test_pairs = readLangs(lang1, lang2, order, data_path, reverse)
+    print("Read %s training sentence pairs" % len(train_pairs))
+    print("Read %s testing sentence pairs" % len(test_pairs))
+
+    if filter_pair:
+        train_pairs = filterPairs(train_pairs, max_length)
+        test_pairs = filterPairs(test_pairs, max_length)
+    print("Trimmed to %s training sentence pairs" % len(train_pairs))
+    print("Trimmed to %s testing sentence pairs" % len(test_pairs))
+
+    print("Constructing training pairs...")
+    max_ngrams_len = 0 
+    for pair in train_pairs:
+        pair[0] =extract_ngrams(pair[1], order)
+        if len(pair[0]) > max_ngrams_len:
+            max_ngrams_len = len(pair[0])
+        uwords = [t.text for t in nlp(str(pair[1]))]
+        pair[1] = len(uwords) # including punctuations
+    print("Constructing test pairs...")
+    for pair in test_pairs:
+        pair[0] = extract_ngrams(pair[1], order)
+        if len(pair[0]) > max_ngrams_len:
+            max_ngrams_len = len(pair[0])
+        uwords = [t.text for t in nlp(str(pair[1]))]
+        pair[1] = len(uwords) # including punctuations
+    print("Max Ngrams length of all training and testing sentences:", max_ngrams_len)
+
+    return input_lang, output_lang, train_pairs, test_pairs, max_ngrams_len
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    
+    input_lang, output_lang, train_pairs, test_pairs, max_ngrams_len = prepareData('eng', 'fra', 
+        args.order, args.data_path, args.filter_pair, args.max_length, True)
+    vocab_ngrams = output_lang.createNGramDictionary()
+    lang = (output_lang.word2index, output_lang.word2count, output_lang.index2word, output_lang.n_words, 
+        args.order, vocab_ngrams, max_ngrams_len)
+
+    with open("pairs%d.pkl" % args.order, 'wb') as f:
+        pkl.dump((train_pairs, test_pairs), f, protocol=pkl.HIGHEST_PROTOCOL) 
+    with open("lang%d.pkl" % args.order, 'wb') as f:
+        pkl.dump(lang, f, protocol=pkl.HIGHEST_PROTOCOL)
+    
+    # with open("lang.pkl", 'rb') as f:
+    #     lang_load = pkl.load(f)
+    # assert(lang_load == lang)
+    
+    print("Example training sentence pairs:")
+    print(random.choice(train_pairs))
+    print("Example testing sentence pairs:")
+    print(random.choice(test_pairs))
