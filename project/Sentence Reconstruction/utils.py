551c8c0e62498c971254050e34a04b6fe48f4e15
X-czh <m1a2@outlook.com>
2018-04-29 01:32:57 -0400

    update

--- project/Sentence Reconstruction/utils.py
+++ project/Sentence Reconstruction/utils.py
@@ -71,5 +71,5 @@ def showPlot(points, args):
     loc = ticker.MultipleLocator(base=0.2)
     ax.yaxis.set_major_locator(loc)
     plt.plot(points)
-    plt.savefig(args.data_path + "loss%d.pdf" % args.order)
+    plt.savefig(args.data_path + "/loss%d.pdf" % args.order)
     plt.close(fig)
