5eec0eb71e18034aaeb98c7a7bc55093f9b7a303
X-czh <m1a2@outlook.com>
2018-05-11 21:18:20 -0400

    finalize

--- project/data/count.py
+++ project/data/count.py
@@ -18,8 +18,8 @@ parser.add_argument('--hpc', action='store_true', default=False,
                     help='set to hpc mode')
 parser.add_argument('--data-path', type=str, default='/scratch/zc807/nlu/data', metavar='PATH',
                     help='data path (default: /scratch/zc807/nlu/data)')
-parser.add_argument('--save-data-path', type=str, default='/scratch/zc807/nlu/sentence_length', metavar='PATH',
-                    help='data path to save pairs.pkl and lang.pkl (default: /scratch/zc807/nlu/sentence_length)')
+parser.add_argument('--save-data-path', type=str, default='/scratch/zc807/nlu/sentence_reconstruction', metavar='PATH',
+                    help='data path to save pairs.pkl and lang.pkl (default: /scratch/zc807/nlu/sentence_reconstruction)')
 parser.add_argument('--order', type=int, default=3, metavar='N',
                     help='order of ngram')
 parser.add_argument('--num-pairs', type=int, default=20000, metavar='N',
@@ -39,10 +39,21 @@ class Lang:
         self.word2count = {}
         self.index2word = {0: "UNK", 1: "SOS", 2: "EOS"}
         self.n_words = 3  # Count UNK, SOS and EOS
+        
+        # for ngrams
+        self.order = order
+        self.vocab0 = OrderedDict()
 
     def addSentence(self, sent):
         for word in sent.split(' '):
             self.addWord(word)
+        ngrams = extract_ngrams(sent, self.order)
+        for ng in ngrams:
+            if ng in self.vocab0:
+                self.vocab0[ng] += 1
+            else:
+                self.vocab0[ng] = 1
+        return ngrams
 
     def addWord(self, word):
         if word not in self.word2index:
@@ -52,14 +63,21 @@ class Lang:
             self.n_words += 1
         else:
             self.word2count[word] += 1
+    
+    def createNGramDictionary(self):
+        tokens = list(self.vocab0.keys())
+        freqs = list(self.vocab0.values())
+        sidx = np.argsort(freqs)[::-1]
+        vocab = OrderedDict([(tokens[s], i) for i, s in enumerate(sidx)])
+        return vocab
 
 def readLangs(lang1, lang2, order, data_path, reverse=False):
     print("Reading lines...")
 
     # Read the file and split into lines
-    train_lines = open(data_path + '/train_fin.txt', encoding='utf-8').\
+    train_lines = open(data_path + '/train.txt', encoding='utf-8').\
         read().strip().split('\n')
-    test_lines = open(data_path + '/test_fin.txt', encoding='utf-8').\
+    test_lines = open(data_path + '/test.txt', encoding='utf-8').\
         read().strip().split('\n')
 
     # Split every line into pairs and normalize
@@ -90,73 +108,41 @@ def prepareData(lang1, lang2, order, data_path, num_pairs, reverse=False):
     print("Trimmed to %s training sentence pairs" % len(train_pairs))
     print("Trimmed to %s testing sentence pairs" % len(test_pairs))
 
-    print("Constructing training pairs...")
+    print("Counting words and constructing training pairs...")
     max_ngrams_len = 0 
-    len_train_pairs = len(train_pairs)
-    for i in range(len_train_pairs):
-        pair = train_pairs[i]
-        output_lang.addSentence(pair[1])
-        pair[0] = pair[1]
-        uwords = [t.text for t in nlp(str(pair[1]))]
-        if len(uwords) < 2:
-            continue
-        if uwords[-1] in ('.', '!', '?'):
-            del uwords[-1] # delete punctuation
-        #print(uwords)
-
-        random_word = random.choice(list(output_lang.index2word.values()))
-        train_pairs.append([pair[0], [random_word], int(random_word in uwords)])
-        pair[1] = [random.choice(uwords)]
-        pair.append(1)
-
-        uwords.remove(pair[1][0])
-        if (len(uwords) > 0):
-            train_pairs.append([pair[0], [random.choice(uwords)], 1])
-
-
-    print("Constructing test pairs...")
-    len_test_pairs = len(test_pairs)
-    for i in range(len_test_pairs):
-        pair = test_pairs[i]
-        pair[0] = pair[1]
+    for pair in train_pairs:
+        #input_lang.addSentence(pair[0])
+        pair[0] = output_lang.addSentence(pair[1])
         if len(pair[0]) > max_ngrams_len:
             max_ngrams_len = len(pair[0])
-        uwords = [t.text for t in nlp(str(pair[1]))]
-        if uwords[-1] in ('.', '!', '?'):
-            del uwords[-1] # delete punctuation
-        random_word = random.choice(list(output_lang.index2word.values()))
-        test_pairs.append([pair[0], [random_word], int(random_word in pair[1])])
-        pair[1] = [random.choice(uwords)]
-        pair.append(1)
+    print("Counted words in training sentences:")
+#     print(input_lang.name, input_lang.n_words)
+    print(output_lang.name, output_lang.n_words)
+    print("Constructing test pairs...")
+    for pair in test_pairs:
+        #input_lang.addSentence(pair[0])
+        pair[0] = extract_ngrams(pair[1], order)
+        if len(pair[0]) > max_ngrams_len:
+            max_ngrams_len = len(pair[0])
+    print("Max Ngrams length of all training and testing sentences:\n", max_ngrams_len)
 
     return input_lang, output_lang, train_pairs, test_pairs, max_ngrams_len
 
 if __name__ == '__main__':
     args = parser.parse_args()
     if not args.hpc:
-        args.data_path = '../../data'
-        args.save_data_path = '.'
-    
-    print("hpc mode: {}".format(args.hpc))
-    print("order: {}".format(args.order))
-    print("num-pairs: {}".format(args.num_pairs))
-    input_lang, output_lang, train_pairs, test_pairs, max_ngrams_len = prepareData('eng', 'fin', 
-        args.order, args.data_path, args.num_pairs, False)
-    lang = (output_lang.word2index, output_lang.word2count, output_lang.index2word, output_lang.n_words)
+        args.data_path = '../data'
+        args.save_data_path ='.'
 
-    with open(args.save_data_path + "/RNNEncoder_train_pairs.pkl", 'wb') as f:
-        pkl.dump(train_pairs, f, protocol=pkl.HIGHEST_PROTOCOL) 
-    with open(args.save_data_path + "/RNNEncoder_test_pairs.pkl", 'wb') as f:
-        pkl.dump(test_pairs, f, protocol=pkl.HIGHEST_PROTOCOL) 
-    with open(args.save_data_path + "/RNNEncoder_lang.pkl", 'wb') as f:
-        pkl.dump(lang, f, protocol=pkl.HIGHEST_PROTOCOL)
-    
-    for i in range(10):
-        print(random.choice(train_pairs))
-        print(random.choice(test_pairs))
+    with open(args.save_data_path + "/lang%d.pkl" % args.order, 'rb') as f:
+        lang = pkl.load(f)
+    (word2index, word2count, index2word, n_words, order, vocab_ngrams, max_ngrams_len) = lang
 
-    print("Example training sentence pairs:")
-    print(random.choice(train_pairs))
-    print("Example testing sentence pairs:")
-    print(random.choice(test_pairs))
-    print("Finished\n")
+    count = [0] * 5
+    for key, value in vocab_ngrams.items():
+        count[len(key.split(" ")) - 1] += 1
+        if len(key.split(" ")) == 5:
+            print(key)
+        if value == 30000:
+            break
+    print(count)
